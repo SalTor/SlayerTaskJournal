abc8f40326c7810a73b092170fee051f5f260bd6
SalTorc <salvatore.torcivia93@gmail.com>
2014-05-20 17:02:48 -0400

    Adjusted get_all and add_task to account for a Jade template I intend on making soon

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
+require("jade");
 var express = require("express");
 var tasks = require("./routes/tasks");
+var path = require("path");
 
 var app = express();
 
 app.configure(function(){
 	app.use(express.logger("dev"));
 	app.use(express.bodyParser());
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 //Nearly all the CRUD functionality; all but delete since I can't delete tasks I've done
-app.get("/tasks", tasks.get_all); //For listing them all out
-app.get("/tasks/:id", tasks.get_by_id); //For looking at the details of each task
-app.post("/task", tasks.add_task); //For adding a new task
+app.get("/tasks", function(req, res){
+	tasks.get_all(function(err, tasks){
+		res.render('allTasks', {title:'Tasks', tasks: tasks});
+	});
+}); //For listing them all out
+
+//app.get("/tasks/:id", , function(req, res){tasks.get_by_id(function(err, tasks){res.render('singleTask', {title:'Task'}); }); ); //For looking at the details of each task
+//Going to focus on looking up a specific task later
+
+app.post("/tasks", function(req, res){
+	var task = req.body;
+	tasks.add_task(task, function(res2){
+		res.send(res2);
+	});
+}); //For adding a new task
 app.put("/tasks/:id", tasks.update_task); //For updating task if a task is prematurely entered into journal
 
 app.listen(3000);
